493520acac6f665abf1cfdf3ecd8ed295d6f07ff
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-27 04:58:23 -0700

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,39 +1,45 @@
 #include "sort.h"
 
-/**
- * swap - swaps 2 int values
- * @array: the integer array to sort
- * @size: the size of the array
- * @a: address of first value
- * @b: address of second value
+
+/** swap - swap two integers using aritmetic operations
+ * without using an intermediate/temporary variable
  *
- * Return: void
+ * @array: array to be printed after swap
+ * @size: size of the array
+ *
+ * @a: pointer to the first integer
+ * @b: pointer to the secod integer
  */
-void swap(int *array, size_t size, int *a, int *b)
+void swap(int array[], size_t size, int *a, int *b)
 {
 	if (*a != *b)
 	{
 		*a = *a + *b;
 		*b = *a - *b;
 		*a = *a - *b;
-		print_array((const int *)array, size);
+
+		print_array((const int *) array, size);
 	}
 }
 
-/**
- * lomuto_partition - partitions the array
- * @array: the integer array to sort
- * @size: the size of the array
- * @lo: the low index of the sort range
- * @hi: the high index of the sort range
+
+
+/** lomuto_partition - partitions the array
  *
- * Return: void
+ * @array: array to be patitioned
+ * @size: size of the array
+ *
+ * @li: lower index of the range to be sorted
+ * @hi: higher index of the range to be sorted
+ *
+ * Return: pivot
  */
-size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
+
+size_t lomuto_partition(int array[], size_t size, ssize_t li, ssize_t hi)
 {
 	int i, j, pivot = array[hi];
 
-	for (i = j = lo; j < hi; j++)
+	for (i = j = li; j < hi; j++)
 		if (array[j] < pivot)
 			swap(array, size, &array[j], &array[i++]);
 	swap(array, size, &array[i], &array[hi]);
@@ -41,36 +47,35 @@ size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
 	return (i);
 }
 
-/**
- * quicksort - quicksorts via Lomuto partitioning scheme
- * @array: the integer array to sort
- * @size: the size of the array
- * @lo: the low index of the sort range
- * @hi: the high index of the sort range
- *
- * Return: void
- */
-void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
-{
-	if (lo < hi)
-	{
-		size_t p = lomuto_partition(array, size, lo, hi);
 
-		quicksort(array, size, lo, p - 1);
-		quicksort(array, size, p + 1, hi);
+/** inner_sort: recursively sort the left and right handsides of a pivot
+ *
+ * @array: array being sorted
+ * @size: length of the array
+ *
+ * @li: lower index of range to be sorted
+ * @hi: higher index of range to be sorted
+ */
+void inner_sort(int array[], size_t size, ssize_t li, ssize_t hi)
+{
+	size_t pivot;
+
+	if (li < hi)
+	{
+		pivot = lomuto_partition(array, size, li, hi);
+
+		inner_sort(array, size, li, pivot - 1);
+		inner_sort(array, size, pivot + 1, hi);
 	}
+		
 }
 
-/**
- * quick_sort - calls quicksort
- * @array: the integer array to sort
- * @size: the size of the array
- *
- * Return: void
- */
+
 void quick_sort(int *array, size_t size)
 {
 	if (!array || !size)
 		return;
-	quicksort(array, size, 0, size - 1);
+
+	inner_sort(array, size, 0, size - 1);
+
 }
