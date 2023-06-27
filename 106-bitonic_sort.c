b4989ac3eea0690384bcbcf283b1c3c5df17c358
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-27 13:01:22 -0700

    bitonic

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,17 +1,25 @@
 #include "sort.h"
 
-/**
- * bitonic_sort - sorts an array following the Bitonic sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
- */
-void bitonic_sort(int *array, size_t size)
-{
-	if (!array || size < 2)
-		return;
 
-	bitonic_recursion(array, 0, size - 1, 1, size);
-}
+/**
+ * bitonic_merge - sorts and merges a sequence in ascending or descending order
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ */
+void bitonic_merge(int *array, int l, int r, int direction)
+{
+	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
+
+	if (r - l >= 1)
+	{
+		for (i = l; i < l + mid; i++)
+		{
+			if (direction == (array[i] > array[i + mid]))
+			{
+				tmp = array[i + mid];
+				array[i + mid] = array[i];
 
 /**
  * bitonic_recursion - recursive function for bitonic sort
@@ -45,26 +53,6 @@ void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 		print_array(array + l, r - l + 1);
 	}
 }
-
-/**
- * bitonic_merge - sorts and merges a sequence in ascending or descending order
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
- * @direction: ascending or descending
- */
-void bitonic_merge(int *array, int l, int r, int direction)
-{
-	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
-
-	if (r - l >= 1)
-	{
-		for (i = l; i < l + mid; i++)
-		{
-			if (direction == (array[i] > array[i + mid]))
-			{
-				tmp = array[i + mid];
-				array[i + mid] = array[i];
 				array[i] = tmp;
 			}
 		}
@@ -72,4 +60,16 @@ void bitonic_merge(int *array, int l, int r, int direction)
 		bitonic_merge(array, step + 1, r, direction);
 	}
 
+}
+/**
+ * bitonic_sort - sorts an array following the Bitonic sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	bitonic_recursion(array, 0, size - 1, 1, size);
 }
